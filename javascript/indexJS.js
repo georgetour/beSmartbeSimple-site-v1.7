f949833689d2af71f1d09fad554fe265715468cd
George Tourtsinakis <tourtsinakis@outlook.com>
2016-08-10 20:05:28 +0300

    Services pages added
    -Services smart and simple pages added
    -No animation yet
    -Some fixes in menu bar and phone pages

---
+++ javascript/indexJS.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $('#servicesLink').click(function () {
+
+        console.log('click works');
+
+        $('.everything').animate({left:'5000px'},3000);
+
+    })
+
+
+  
+
+
+});
