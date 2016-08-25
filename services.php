ec6b6c380312a94ba8a645f9abca674e8b27cee4
George Tourtsinakis <tourtsinakis@outlook.com>
2016-08-25 21:53:06 +0300

    Moved services test page to normal page

--- services.php
+++ services.php
@@ -14,9 +14,9 @@
 <!--Services page-->
 <div id="showServices" class=" textColor">
 
-    <div class="servicesRow">
+    <div class="servicesRow ">
         <!--First row-->
-        <div class=" alignCenter">
+        <div class=" alignCenter container-fluid">
             <!--Column 1-->
             <div   class="everything col-lg-3 col-md-3 col-sm-12 col-xs-12  ">
                 <h4 class="bold " >Services</h4>
@@ -27,94 +27,101 @@
                 <h3 class="bold">I can make small to medium websites for you or your company.
                 </h3>
 
-                <h4 class=" bold">Smart</h4>
+                <h4 class="yourSiteWillBe bold">Your site will be :</h4>
             </div>
             <!--Column 3-->
             <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
-                <div></div>
+                <h2></h2>
             </div>
         </div>
         <!--End first row-->
 
         <!--Second row-->
-        <div class="container-fluid alignCenter ">
-            <!--Column 1-->
-            <div   class=" pictures everything col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
-                <p>Your site will look awesome in all devices and browsers.</p>
+        <div class="container    ">
 
-                <div class=" "><img class="img-responsive picturesIcons" src="images/Responsive-Web-Design-compressor.png"></div>
-            </div>
+            <div class="text-center"><a class="servicesTags">Responsive <span>,</span></a><a class="servicesTags">Ultra Fast</a></div>
+            <div class="text-center"><a class="servicesTags">Search Engine Optimized <span>,</span></a><a class="servicesTags">Easy to Navigate</a></div>
+            <div class="text-center"><a class="servicesTags"><a class="servicesTags">Customizable</a></div>
 
-            <!--Column 2-->
-            <div class="pictures col-lg-6  col-md-6 col-sm-12 col-xs-12  everything ">
-                <p>It will be ultra fast since I use Cloud Private Servers.</p>
-                <br>
-                <div ><img class="img-responsive picturesIcons" src="images/Cloud_computing.png"></div>
+            <div class="row imageRowServices alignCenter ">
 
-            </div>
-            <!--Column 3-->
-            <div class="pictures col-lg-3 col-md-3 col-sm-12 col-xs-12  ">
-                <p>Search engines will find it.<br>
-                    People will find it.
+                <!--Left column-->
+                <div class="col-lg-4  col-md-4 col-sm-12 col-xs-12  borderTest outsideHeight">
+                    <h3>It will look awesome in all devices and browsers</h3>
+                    <div class=""><img alt="Responsive Web Design Picture"  class="img-responsive picturesIcons "  src="images/Responsive-Web-Design-compressor.png">  </div>
+                <h4 class="bold">Using Twitter Bootstrap</h4>
+                <p>One of the most popular tools used by thousands  developers
+                    so it can make responsive websites .As more and more people use internet
+                    from their mobiles your site must work flawlessly  everywhere.
                 </p>
-                <div ><img class="img-responsive picturesIcons2" src="images/Search_engine_optimization_copy-compressor.png"></div>
-            </div>
-        </div>
-        <!--End second row-->
+                </div>
 
+                <!--Middle Column-->
+                    <div class="col-lg-4 col-md-4  col-md-12 col-sm-12 col-xs-12 borderTest outsideHeight">
+                        <h3>High speed with Cloud Private Hosting </h3>
+                        <div class=""><img alt="Cloud Computing"  class="img-responsive picturesIcons " src="images/Cloud_computing.png">  </div>
+                        <h4 class="bold">Cloud Virtual Private Server</h4>
+                        <p>The power of a cloud Virtual Private Server will host your site.
+                            That means that your site will NOT drop or disappoint anyone with its speed and availability.
 
-        <!--First row-->
-        <div class="container-fluid alignCenter">
-            <!--Column 1-->
-            <div   class="everything col-lg-3 col-md-3 col-sm-12 col-xs-12  ">
-                <h4 class="bold " ></h4>
-            </div>
+                        </p></br>
+                    </div>
 
-            <!--Column 2-->
-            <div class="col-lg-6  col-md-6 col-sm-12 col-xs-12  everything ">
+                <!--Right Column-->
+                <div class=" col-lg-4  col-md-4 col-sm-12 col-xs-12  borderTest outsideHeight">
+                    <h3>Simple navigation.No need for compass </h3>
+                    <div class=""> <img alt="Compass" class="img-responsive picturesIcons " src="images/free_old_compass_by_beowulf.png">  </div>
 
+                    <h4 class="bold">User friendly priority</h4>
+                    <p>Your visitors don't like complicated things.
+                        They want to know what your site does and find it easily.
+                        I keep everything simple as possible and with minor clicks people will explore
+                        and enjoy your site.
+                    </p>
 
-                <h3 class=" bold">Simple</h3>
-            </div>
-            <!--Column 3-->
-            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
-                <div></div>
-            </div>
-        </div>
-        <!--End first row-->
-
-        <!--Second row-->
-        <div class="container-fluid alignCenter servicesRow ">
-            <!--Column 1-->
-            <div   class="col-lg-2 col-md-2 col-sm-12 col-xs-12 ">
-
+                </div>
             </div>
 
-            <!--Column 2-->
-            <div class=" col-lg-8  col-md-8 col-sm-12 col-xs-12   ">
-                <h2 class="bold">Simple means Beautiful,Powerful and Fast.</h2>
-                <p class="imagesShadows servicesSimple col-lg-6  col-md-6 col-sm-12 col-xs-12">Your visitors don't like complicated things.They want to know what your site does
-                    and find it easily.
-                </p>
-                <p class=" servicesSimple imagesShadows col-lg-6  col-md-6 col-sm-12 col-xs-12 ">I keep everything simple as possible and with minor clicks people will navigate
-                    and enjoy your site.
+        </div><!--End second row-->
 
-                </p>
-                <br>
+            <div class="container" >
+                <div class="row imageRowServices alignCenter ">
 
+                    <!--Left column-->
+                    <div class="col-lg-6  col-md-6 col-sm-12 col-xs-12  borderTest outsideHeight">
+                        <h3>Google will find it, people will find it.</h3>
+                        <div class=""><img alt="Search Engine Optimization"  class="img-responsive picturesIcons "  src="images/Search_engine_optimization_copy-compressor.png">  </div>
+                        <h4 class="bold">SEO</h4>
+                        <p>If you want your website to rank as  high as possible to search engines for future visitors or customers,
+                             then you need Search Engine Optimization.A lot of factors take place
+                            and we will have really good results .
+                        </p>
+                    </div>
+
+                    <!--Right Column-->
+                    <div class="col-lg-6 col-md-6  col-md-12 col-sm-12 col-xs-12 borderTest outsideHeight ">
+                        <h3>Customizable by You </h3>
+                        <div class=""><img alt="Wordpress logo" class="img-responsive picturesIcons " src="images/wordpressLogo.png">  </div>
+                        <h4 class="bold">1/3 of world sites use Wordpress</h4>
+                        <p>Wordpress is a very powerful Custom Management System that will give you the ability
+                            to dynamically add content to your site and update it without any programming.
+                            Currently,I'm working with some extra features and I'm not able to provide you with a Wordpress site.
+
+
+                        </p>
+                    </div>
+
+                </div>
 
             </div>
-            <!--Column 3-->
-            <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12  ">
 
-            </div>
-        </div>
-        <!--End second row-->
-    </div>
+</div>
+
+
     <!--footer-->
-    <?php include "footerStatic.php" ?>
+    <?php include "footerPortfolio.php" ?>
 
-</div><!--End services page -->
+
 
 
 </body>
