5c6581ceebac06c1f02c70057bcc47e5f0e0feaf
George Tourtsinakis <tourtsinakis@outlook.com>
2016-08-26 21:09:47 +0300

    Centered awards image for bigger resolutions
    -Centered awards image for bigger resolutions

--- awards.php
+++ awards.php
@@ -37,6 +37,7 @@
 
             </div>
             <div style="margin-top: 10px" class="col-lg-8  col-md-8 col-sm-12 col-xs-12 alignCenter ">
+               <div><img class="img-responsive centerImg" src="images/awards/awardsPhotoshopEditing.jpg"></div>
             </div>
             <div   class=" col-lg-2 col-md-2 col-sm-12 col-xs-12  "></div>
         </div>
