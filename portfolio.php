3065a133447fb9a44727d742ea994293fe1022e8
George Tourtsinakis <tourtsinakis@outlook.com>
2016-12-05 14:18:16 +0200

    Update portfolio with mywordpress

--- portfolio.php
+++ portfolio.php
@@ -51,6 +51,25 @@
 
         </div><!--Farmaparnassou Row -->
 
+        <!--Test Wordpress Row -->
+        <div style="margin-bottom:10px " class="imageRow row alignCenter ">
+            <!--Left column-->
+            <div class="col-lg-9  col-md-9 col-sm-12 col-xs-12   showEffect5 ">
+                <a href="http://www.mywordpress.gr" target="_blank">
+                    <img class="img-responsive borderPortfolio" src="images/portfolio/mywordpress-compressor.jpg" alt="Test Wordpress">
+                </a>
+            </div>
+            <!--Right Column-->
+            <div class=" col-lg-3  col-md-3 col-sm-12 col-xs-12   showEffect6 ">
+                <h4 class="bold"> <a href="http://www.mywordpress.gr" target="_blank">MyWordpress.gr</a> </h4>
+                <p> This is a project that uses Wordpress and Bootstrap custom theme creation.
+                </p>
+                <p>I can create custom themes in Wordpress at your needs as you can see. </p>
+                <img class="img-responsive imagesShadows phonePortfolio" src="images/portfolio/mywordpresstrimmed-compressor.png" alt="Test Wordpress at mobile">
+            </div>
+
+        </div><!--Test Wordpress End-->
+
         <!--Wikipedia project row-->
         <div class="imageRow row alignCenter ">
             <div class="col-lg-9  col-md-9 col-sm-12 col-xs-12  showEffect3">
@@ -128,24 +147,7 @@
 
         </div><!--Atypon Row End-->
 
-        <!--Test Wordpress Row -->
-        <div style="margin-bottom:10px " class="imageRow row alignCenter ">
-            <!--Left column-->
-            <div class="col-lg-9  col-md-9 col-sm-12 col-xs-12   showEffect5 ">
-                <a href="https://github.com/georgetour/TestWordpress" target="_blank">
-                    <img class="img-responsive borderPortfolio" src="images/portfolio/test-wordpress-compressor.png" alt="Test Wordpress">
-                </a>
-            </div>
-            <!--Right Column-->
-            <div class=" col-lg-3  col-md-3 col-sm-12 col-xs-12   showEffect6 ">
-                <h4 class="bold"> <a href="https://github.com/georgetour/TestWordpress" target="_blank">Test Wordpress</a> </h4>
-                <p> This is a project under construction that uses Wordpress and Bootstrap custom theme creation.'
-                </p>
-                <p>Customers will be able to see what Wordpress is and how it works.   </p>
-                <img class="img-responsive imagesShadows phonePortfolio" src="images/portfolio/testwordpresstrimmed-compressor.png" alt="Test Wordpress at mobile">
-        </div>
 
-        </div><!--Test Wordpress End-->
 
 
         <!--Image share Row-->
